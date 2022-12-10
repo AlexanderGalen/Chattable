fb6a3bc42d2264daea916ef5fee9f826f9216aec
Alexander Carr <alexanderc93@gmail.com>
2022-12-10 15:20:34 -0800

    update db connection for new deploy

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.DB_URI) {
+  sequelize = new Sequelize(process.env.DB_URI);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
