6a9cf7f2aa5b1f190e1d88c713daec77aa711879
Alexander Carr <alexanderc93@gmail.com>
2022-12-12 17:09:15 -0800

    update env vars for railway

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
-const Sequelize = require('sequelize-cockroachdb');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
 
-if (process.env.DB_URI) {
-  sequelize = new Sequelize(process.env.DB_URI);
+if (process.env.MYSQL_URL) {
+  sequelize = new Sequelize(process.env.MYSQL_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
